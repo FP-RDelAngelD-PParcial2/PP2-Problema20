928d60d26f8e280fced51f2952218bd4c8a17673
PC <PC@192.168.1.70>
2016-10-17 19:12:34 -0500

    20.	Realiza un programa que dados dos números determine si son amigos. Dos números son amigos cuando la suma de sus divisores propios (múltiplos) es igual al otro número y viceversa.

--- src/pp2/problema20/PP2Problema20.java
+++ src/pp2/problema20/PP2Problema20.java
@@ -34,26 +34,36 @@ public class PP2Problema20 {
     public static void numerosAmigos(){ //Dos números enteros positivos A y B 
                                         //son números amigos si la suma de los divisores propios de A es igual a B 
                                         //y la suma de los divisores propios de B es igual a A
-        int a, b, suma;
+        int a, b, suma, suma2;
         suma = 0;
+        suma2 = 0;
         a=verificarNumero("numero 1 "); //Se solicita el primer numero //284
         b=verificarNumero("numero 2 "); //Se solicita el segundo numero //220
         for(int i=1;i<a;i++){//Ciclo para encontrar los divisores del numero 1
                              //i se inicia en 1, mientras que i sea menor que a, i aumentara 1
                              //i=1 1<284
+                             //i=2 2<284
+                             //i=3 3<284
+                             //i=4 4<284
             if(a%i==0){    //Si el residuo de a entre i es igual a 0
+                           //284%1==0 VERDADERO
+                           //284%2==0 VERDADERO
+                           //284%3==0 FALSO (sale del ciclo y entra al for)
+                           //284%4==0 VERDADERO
                 suma=suma+i;//En la variable suma, se iran sumando los valores divisores del numero
+                            //suma=0+1 suma=1
+                            //suma=1+2 suma=3
+                            //suma=3+4 suma=7
             }
         }
         if(suma==b){
-            suma=0;
             for(int i=1;i<b;i++){
                 if(b%i==0){
-                    suma=suma+i;
+                    suma2=suma2+i;
                 }
             }
         }
-        if(suma==a){
+        if(suma2==a){
             System.out.print("Los numeros " + a + " y " + b + " son amigos");
         }else{
             System.out.print("Los numeros " + a + " y " + b + " no son amigos");
